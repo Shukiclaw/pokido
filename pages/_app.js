278778c0cb5ee4d88211922c46d3a5bb0a787fe4
Shuki <shuki@clawdbot.local>
2026-02-15 07:38:13 +0000

    Add retro Pokemon Game Boy style to inner screen buttons and menus
    - Added Press Start 2P font from Google Fonts
    - Created retro.css with Game Boy / NES style components
    - Updated buttons with pixel-perfect shadows and retro aesthetics
    - Applied 8-bit style to menu options, scan buttons, and navigation
    - Maintained green screen background and outer pokedex design
    - All functionality preserved

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,23 @@
 import '../styles/globals.css';
+import '../styles/retro.css';
+import Head from 'next/head';
 import { LanguageProvider } from '../contexts/LanguageContext';
 import { CollectionProvider } from '../contexts/CollectionContext';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <LanguageProvider>
-      <CollectionProvider>
-        <Component {...pageProps} />
-      </CollectionProvider>
-    </LanguageProvider>
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet" />
+      </Head>
+      <LanguageProvider>
+        <CollectionProvider>
+          <Component {...pageProps} />
+        </CollectionProvider>
+      </LanguageProvider>
+    </>
   );
 }
 
